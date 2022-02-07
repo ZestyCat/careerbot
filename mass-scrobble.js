5bf0bd74f1265ab455c2df4c3a8fa57587824384
Gregory Bizup <g.bizup@gmail.com>
2022-02-07 13:12:12 -0800

    color changing bars, song titles, username input

--- mass-scrobble.js
+++ mass-scrobble.js
@@ -1,6 +1,6 @@
-// Hivemind scrobbler
-
 const users = []
+const plotButton = document.getElementById('plot')
+const username = document.getElementById('userInput')
 
 const getTopTracks = async (user) => {
 	const data = await sendHttpRequest('GET', `${ROOT_URL}?method=user.gettoptracks&user=${user}&api_key=${api_key}&format=json`)	
@@ -11,11 +11,15 @@ const getTopTracks = async (user) => {
 
 const plot = async () => {
 	
-	const data = await getTopTracks('g_biz')
+	const data = await getTopTracks(username.value)
 
-	let width = 500
+	let width = 300
 	let barHeight = 20 
 	let height = (barHeight + 2) * data.length
+
+	d3.select('#divplot')
+	.selectAll('svg')
+	.remove()
 
 	let xScale = d3.scaleLinear()
 		.domain([0, Math.max(...data.map(d => { return d.playcount }))])
@@ -24,17 +28,17 @@ const plot = async () => {
 	let yScale = d3.scaleLinear()
 		.domain([0, data.length])
 		.range([0, height])
-
+	
 	let plot = d3.select("#divplot")
 		.append('svg:svg')
-		.attr('width', width)
+		.attr('width', width*3)
 		.attr('height', height)
 	
 	plot.selectAll('rect')
 	.data(data)
 	.enter()
 	.append('svg:rect')
-	.attr('x', xScale(0))
+	.attr('x', 0.75 * width)
 	.attr('y', (datum, index) => { return yScale(index) })
 	.attr('width', datum => { return xScale(datum.playcount) })
 	.attr('height', barHeight )
@@ -44,29 +48,42 @@ const plot = async () => {
 	.data(data)
 	.enter()
 	.append('svg:text')
-	.attr('x', datum => { return xScale(datum.playcount) })
+	.attr('x', datum => { return xScale(datum.playcount) + 0.75 * width })
 	.attr('y', (datum, index) => { return yScale(index) })
-	.attr('dy', barHeight/1.3)
+	.attr('dy', barHeight/1.5)
 	.attr('dx', '-0.5em')
 	.attr('text-anchor', 'end')
-	.attr("style", "font-size: 6; font-family: Times New Roman")
+	.attr("style", `font-size: ${barHeight/1.5}; font-family: Times New Roman`)
 	.text(datum => { return datum.playcount })
 	.attr('fill', 'white')
-/*
-	plot.selectAll('text.xAxis')
+
+	plot.selectAll('text.yAxis')
 	.data(data)
 	.enter()
 	.append('svg:text')
-	.attr('x', (datum, index) => { return xScale(index) + barHeight })
-	.attr('y', height)
-	.attr('dx', -barHeight/2)
-	.attr('text-anchor', 'middle')
-	.attr("style", "font-size: 10; font-family: Times New Roman")
-	.text(datum => { return datum.name })
+	.attr('x', 0.75 * width)
+	.attr('y', (datum, index) => { return yScale(index)})
+	.attr('dy', barHeight/1.5)
+	.attr('dx', '-0.5em')
+	.attr('text-anchor', 'end')
+	.attr("style", `font-size: ${barHeight/1.5}; font-family: Times New Roman`)
+	.text(datum => { return `${datum.name}` })
 	.attr('class', 'yAxis')
 	.attr('fill', 'white')
-*/
+
+	plot.selectAll('rect')
+	.on('mouseover', handleMouseOver)
+	.on('mouseout', handleMouseOut)
 }
 
+function handleMouseOver(d, i) {
+	d3.select(this)
+	.attr('fill', 'orange')
+}
 
-plot()
+function handleMouseOut(d, i) {
+	d3.select(this)
+	.attr('fill', '#2d578b')
+}
+
+plotButton.addEventListener('click', () => { plot() })
