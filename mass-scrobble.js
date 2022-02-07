7badb590aba9dae4bf4375a421c3a9f89112d772
Gregory Bizup <g.bizup@gmail.com>
2022-02-07 14:54:15 -0800

    hyperlinks

--- mass-scrobble.js
+++ mass-scrobble.js
@@ -1,6 +1,6 @@
 const users = []
 const plotButton = document.getElementById('plot')
-const username = document.getElementById('userInput')
+const userField = document.getElementById('userInput')
 
 const getTopTracks = async (user) => {
 	const data = await sendHttpRequest('GET', `${ROOT_URL}?method=user.gettoptracks&user=${user}&api_key=${api_key}&format=json`)	
@@ -8,18 +8,18 @@ const getTopTracks = async (user) => {
 	return(json['toptracks']['track'])
 }
 
-
-const plot = async () => {
-	
-	const data = await getTopTracks(username.value)
-
-	let width = 300
-	let barHeight = 20 
-	let height = (barHeight + 2) * data.length
-
+const clear = () => {
 	d3.select('#divplot')
 	.selectAll('svg')
 	.remove()
+}
+
+const plot = async (user) => {
+	const data = await getTopTracks(user)
+
+	let width = 100
+	let barHeight = 20 
+	let height = (barHeight + 2) * data.length
 
 	let xScale = d3.scaleLinear()
 		.domain([0, Math.max(...data.map(d => { return d.playcount }))])
@@ -31,24 +31,28 @@ const plot = async () => {
 	
 	let plot = d3.select("#divplot")
 		.append('svg:svg')
-		.attr('width', width*3)
+		.attr('width', width*4)
 		.attr('height', height)
-	
+
 	plot.selectAll('rect')
 	.data(data)
 	.enter()
-	.append('svg:rect')
-	.attr('x', 0.75 * width)
-	.attr('y', (datum, index) => { return yScale(index) })
-	.attr('width', datum => { return xScale(datum.playcount) })
-	.attr('height', barHeight )
-	.attr('fill', '#2d578b')
+	.append('a')
+	.attr('xlink:href', datum => { return datum.url })
+		.append('svg:rect')
+		.attr('x', xScale(0))
+		.attr('y', (datum, index) => { return yScale(index) })
+		.attr('width', datum => { return xScale(datum.playcount) })
+		.attr('height', barHeight )
+		.attr('fill', '#2d578b')
+		.attr('class', (datum, index) => { return user + index })
+		.attr('rank', (datum, index) => { return user + index })
 
 	plot.selectAll('text')
 	.data(data)
 	.enter()
 	.append('svg:text')
-	.attr('x', datum => { return xScale(datum.playcount) + 0.75 * width })
+	.attr('x', datum => { return xScale(datum.playcount) })
 	.attr('y', (datum, index) => { return yScale(index) })
 	.attr('dy', barHeight/1.5)
 	.attr('dx', '-0.5em')
@@ -57,33 +61,47 @@ const plot = async () => {
 	.text(datum => { return datum.playcount })
 	.attr('fill', 'white')
 
+	console.log(data)
+
 	plot.selectAll('text.yAxis')
 	.data(data)
 	.enter()
-	.append('svg:text')
-	.attr('x', 0.75 * width)
-	.attr('y', (datum, index) => { return yScale(index)})
-	.attr('dy', barHeight/1.5)
-	.attr('dx', '-0.5em')
-	.attr('text-anchor', 'end')
-	.attr("style", `font-size: ${barHeight/1.5}; font-family: Times New Roman`)
-	.text(datum => { return `${datum.name}` })
-	.attr('class', 'yAxis')
-	.attr('fill', 'white')
+	.append('a')
+	.attr('xlink:href', datum => { return datum.url })
+		.append('svg:text')
+		.attr('x', datum => { return xScale(datum.playcount)})
+		.attr('y', (datum, index) => { return yScale(index)})
+		.attr('dy', barHeight/1.5)
+		.attr('dx', '0.5em')
+		.attr('text-anchor', 'start')
+		.attr("style", `font-size: ${barHeight/1.5}; font-family: Times New Roman`)
+		.text(datum => { return `${datum.artist.name} - ${datum.name}` })
+		.attr('class', 'yAxis')
+		.attr('mbid', datum => datum.mbid)
+		.attr('fill', '#2d578b')
+		.attr('text-decoration', 'underline')
+		.attr('class', (datum, index) => { return `${user + index} yAxis` })
+		.attr('rank', (datum, index) => { return user + index })
+		
 
-	plot.selectAll('rect')
+	plot.selectAll('rect, text.yAxis')
 	.on('mouseover', handleMouseOver)
 	.on('mouseout', handleMouseOut)
 }
 
 function handleMouseOver(d, i) {
-	d3.select(this)
+	let thisRank = this.getAttribute('rank')
+	d3.select('#divplot')
+	.selectAll('rect, text.yAxis')
+	.attr('fill', '#2d578b')
+	d3.selectAll(`.${thisRank}`)
 	.attr('fill', 'orange')
 }
 
 function handleMouseOut(d, i) {
-	d3.select(this)
+	d3.select('#divplot')
+	.selectAll('rect, text.yAxis')
 	.attr('fill', '#2d578b')
 }
 
-plotButton.addEventListener('click', () => { plot() })
+plotButton.addEventListener('click', () => { clear(), plot(userField.value) })
