5e89f941dc3b96ffb9380ab720a656d14e556887
greg <g.bizup@gmail.com>
2022-08-30 14:06:47 -0700

    filter sentence by word list

--- corpus/get_eeo_terms.py
+++ corpus/get_eeo_terms.py
@@ -2,6 +2,7 @@ import pandas as pd
 import yake
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
 
 print("getting stopwords...")
 stops = set(stopwords.words("english"))
@@ -10,11 +11,13 @@ stops = set(stopwords.words("english"))
 print("getting eeo terms...")
 with open("./data/EEO.txt") as f:
     eeo = f.read().lower()
-    kwx_eeo_term = yake.KeywordExtractor(n=1, top=100)
-    kwx_eeo_phrase = yake.KeywordExtractor(n=3, top=100)
-    porter = PorterStemmer()
-    eeo_terms = [ngram[0] for ngram in kwx_eeo_term.extract_keywords(eeo)]
-    eeo_phrases = [ngram[0] for ngram in kwx_eeo_phrase.extract_keywords(eeo)]
+    #kwx_eeo_term = yake.KeywordExtractor(n=1, top=100)
+    #kwx_eeo_phrase = yake.KeywordExtractor(n=3, top=100)
+    #porter = PorterStemmer()
+    #eeo_terms = [ngram[0] for ngram in kwx_eeo_term.extract_keywords(eeo)]
+    #eeo_phrases = [ngram[0] for ngram in kwx_eeo_phrase.extract_keywords(eeo)]
+    tokens = word_tokenize(eeo)
+    eeo_terms = [t for t in tokens if t not in stops and t.isalpha()]
 
-with open("eeo_terms.txt", "w") as f:
-    f.write(" ".join(eeo_terms + eeo_phrases))
+with open("./data/eeo_terms.txt", "w") as f:
+        f.write(" ".join(eeo_terms))
--- corpus/parse_listings.py
+++ corpus/parse_listings.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import json
+from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
 from nltk.probability import FreqDist
 import nltk
 
@@ -11,29 +13,46 @@ jobs["company"] = jobs["company"].str.lower()
 jobs["title"] = jobs["title"].str.lower()
 jobs["description"] = jobs["description"].str.lower()
 
-# Tokenize
-tokens = [word for word in word_tokenize(jobs[jobs["company"].str.contains("amazon")]["description"].to_string()) \
-        if word.isalpha()]
-
-# get list of eeo terms
+# get stopwords
 with open("data/eeo_terms.txt") as f:
     eeo = f.read().split(" ")
+with open("data/common.txt") as f:
+    common = f.read().split(" ")
+stops = " ".join(set(stopwords.words("english") + eeo + common))
 
-# get stopwords 
-stops = set(stopwords.words("english") + eeo)
+# Tokenize
+company, description = "amazon", "engineer"
+selection = jobs[jobs["company"].str.contains(company)] \
+        [jobs["description"].str.contains(description)]
+tokens = [word for word in word_tokenize(selection["description"].to_string()) \
+        if word.isalpha()]
+
+# get eeo-related sentences to discard
+sentences = sent_tokenize(" ".join(selection["description"].to_list()))
+def filter_sentences(sentences, filter_words, tolerance = .20):
+    """  Remove sentences if they have too high ratio of listed terms """
+    filtered = []
+    for s in sentences:
+        tok = word_tokenize(s)
+        n = set(t for t in tok).intersection(e for e in eeo)
+        ratio = len(n) / len(tok)
+        if ratio > tolerance:
+            continue
+        if ratio < tolerance:
+            filtered.append(s)
+    return filtered
 
 # tag and filter tokens
 tag_filt = [tok for tok in nltk.pos_tag(tokens) if tok[0] not in stops]
-
 key_tags = pd.DataFrame({"word" : [tok[0] for tok in tag_filt],
                    "tag" : [tok[1] for tok in tag_filt]})
-
 key_list = key_tags[key_tags["tag"].str.contains("NN")]["word"].to_list()
 
-trigrams = nltk.trigrams(tokens)
 
-fdist_1 = FreqDist(key_list).most_common(100)
+porter = PorterStemmer()
+trigrams = nltk.trigrams([t for t in tokens if porter.stem(t) not in stops])
 
+fdist_1 = FreqDist(key_list).most_common(50)
 fdist_3 = FreqDist(trigrams).most_common(20)
 
 print(fdist_1)
